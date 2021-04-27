6f3519cbd80b3d8e443301af83b20d1677a141a1
A514527 <A514527@DESKTOP-3VLUGTF>
2021-04-27 13:07:10 +0200

    feat. update configuration file, constants and properties for SK-RI setup

---
+++ src/main/java/com/i3market/sdk/ri/execution_patterns/SdkRiProperties.java
@@ -0,0 +1,10 @@
+package com.i3market.sdk.ri.execution_patterns;
+
+// SDK RI Properties
+public class SdkRiProperties {
+	
+	public static final String BACKPLANE_URL = "backplane.url";
+	public static final String OIDC_URL= "oidc.url";
+	public static final String VC_URL = "verifiable_credentials.url";
+	
+}
