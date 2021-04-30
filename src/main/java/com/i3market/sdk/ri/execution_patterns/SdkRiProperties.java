b903705d5ebc6db2a949f7869f3972bb18dbcd83
A514527 <A514527@DESKTOP-3VLUGTF>
2021-04-30 13:25:43 +0200

    feat. new properties to support data access and wallet endpoints

--- src/main/java/com/i3market/sdk/ri/execution_patterns/SdkRiProperties.java
+++ src/main/java/com/i3market/sdk/ri/execution_patterns/SdkRiProperties.java
@@ -6,5 +6,7 @@ public class SdkRiProperties {
 	public static final String BACKPLANE_URL = "backplane.url";
 	public static final String OIDC_URL= "oidc.url";
 	public static final String VC_URL = "verifiable_credentials.url";
+	public static final String DATA_ACCESS_URL = "data_access.url";
+	public static final String SW_WALLET_URL = "sw_wallet.url";
 	
 }
