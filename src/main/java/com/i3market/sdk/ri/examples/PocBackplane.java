cced201476073f39450fb280397c0a931f7ad930
A514527 <A514527@DESKTOP-3VLUGTF.mshome.net>
2021-05-10 12:26:53 +0200

    feat.: update including JWT query param in rest methods.

--- src/main/java/com/i3market/sdk/ri/examples/PocBackplane.java
+++ src/main/java/com/i3market/sdk/ri/examples/PocBackplane.java
@@ -1,10 +1,15 @@
 package com.i3market.sdk.ri.examples;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.i3m.api.ApiClient;
 import com.i3m.api.ApiException;
 import com.i3m.api.ApiResponse;
 import com.i3m.api.Configuration;
+import com.i3m.api.auth.Authentication;
 import com.i3m.api.auth.HttpBasicAuth;
+import com.i3m.api.auth.HttpBearerAuth;
 import com.i3m.api.backplane.HelloControllerApi;
 import com.i3m.api.backplane.PingControllerApi;
 import com.i3m.model.backplane.HelloResponse;
@@ -17,7 +22,7 @@ public class PocBackplane {
 		
 	}
 	
-	public PingResponse ping () throws ApiException {
+	public PingResponse ping (String jwt) throws ApiException {
 		
 		String basePath = SdkRiConstants.BACKPLANE_ENDPOINT;
 		
@@ -30,10 +35,12 @@ public class PocBackplane {
 		// Avoiding default server conf based on localhost url
 		defaultClient.setServerIndex(null);
 		
-		// TO-DO Configure JWT Authentication
-        // HttpBasicAuth BasicAuth = (HttpBasicAuth) defaultClient.getAuthentication("BasicAuth");
-        // BasicAuth.setUsername("YOUR USERNAME");
-        // BasicAuth.setPassword("YOUR PASSWORD");
+		// Setup authentications (JWT).
+		Map<String, Authentication> authentications = defaultClient.getAuthentications();
+		authentications = new HashMap<String, Authentication>();
+		HttpBearerAuth jwtAuth = new  HttpBearerAuth(null);
+		jwtAuth.setBearerToken(jwt);
+		authentications.put("JWTAuth", jwtAuth);
 		
 		PingControllerApi controller = new PingControllerApi();
 		return  controller.pingControllerPing();
