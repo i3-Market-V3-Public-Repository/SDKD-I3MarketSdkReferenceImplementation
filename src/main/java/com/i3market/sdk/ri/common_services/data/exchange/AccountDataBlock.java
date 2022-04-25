2795ec1aee3051ad1ae5e282efee179641242fe2
NinesSanguino <maria.sanguino@atos.net>
2022-04-25 15:04:33 +0000

    Update src/main/java/com/i3market/sdk/ri/bean/package-info.java, src/main/java/com/i3market/sdk/ri/common_services/accounting/package-info.java, setup-sdk-ri.sh, src/main/java/com/i3market/sdk/ri/common_services/data/discovery/RetrieveOfferingTemplate.java, src/main/java/com/i3market/sdk/ri/common_services/data/discovery/RetrieveOfferingList.java, src/main/java/com/i3market/sdk/ri/common_services/data/discovery/RetrieveOfferingByProviderId.java, src/main/java/com/i3market/sdk/ri/common_services/data/discovery/RetrieveOfferingById.java, src/main/java/com/i3market/sdk/ri/common_services/data/discovery/RetrieveOfferingByCategory.java, src/main/java/com/i3market/sdk/ri/common_services/data/discovery/RetrieveCategoryList.java, src/main/java/com/i3market/sdk/ri/common_services/alerts/subscriptions/GetSubscriptions.java, src/main/java/com/i3market/sdk/ri/common_services/data/exchange/AccountDataBlock.java

--- src/main/java/com/i3market/sdk/ri/common_services/data/exchange/AccountDataBlock.java
+++ src/main/java/com/i3market/sdk/ri/common_services/data/exchange/AccountDataBlock.java
@@ -86,9 +86,9 @@ public class AccountDataBlock {
 	private static Payload decrypted_cipherblock;
 	private static String sendJwk;
 
-	static String privateKeyStrConsumer = "{\"kty\":\"EC\",\"crv\": \"P-256\",\"x\": \"NjyNt9n_QEsjwPDQJp45vNn7dn3JeMc8Qj1ffDWfxLA\",\"y\": \"hzj_0heDulDkvAR_u1SqkKVi98_K1MX0GztJggAQ8PQ\",\"d\": \"-VAvKlK5_0-B4c82klsXri28pGbYx2iwKPryoKEE1f4\"}";
-	static String publicKeyStrConsumer = "{\"kty\":\"EC\",\"crv\": \"P-256\",\"x\": \"NjyNt9n_QEsjwPDQJp45vNn7dn3JeMc8Qj1ffDWfxLA\",\"y\": \"hzj_0heDulDkvAR_u1SqkKVi98_K1MX0GztJggAQ8PQ\"}";
-	static String publicKeyStrProvider = "{\"kty\":\"EC\",\"crv\": \"P-256\",\"x\": \"NjyNt9n_QEsjwPDQJp45vNn7dn3JeMc8Qj1ffDWfxLA\",\"y\": \"hzj_0heDulDkvAR_u1SqkKVi98_K1MX0GztJggAQ8PQ\"}";
+	static String privateKeyStrConsumer = "{}";
+	static String publicKeyStrConsumer = "{}";
+	static String publicKeyStrProvider = "{}";
 
 	static ECKey publicKeyProvider;
 	static {
