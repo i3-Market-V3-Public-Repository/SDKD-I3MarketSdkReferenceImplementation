b12e7e675d9494b580a4d07fa53bea8bac953922
A514527 <A514527@DESKTOP-3VLUGTF.mshome.net>
2023-01-31 13:16:04 +0100

    fix on scm agreements interface

--- src/main/java/com/i3market/sdk/ri/resource/SdkRiHub.java
+++ src/main/java/com/i3market/sdk/ri/resource/SdkRiHub.java
@@ -1271,7 +1271,7 @@ public class SdkRiHub {
 	@ApiOperation(value = "retrieve the agreement by consumer", tags = "common-services: contract")
 	@ApiResponses(value = { @ApiResponse(code = 400, message = "failed to get the agreements") })
 	@Produces({"application/json"})
-	public com.i3m.api.ApiResponse<ScManagerOasActiveAgreements> checkAgreementsByConsumer(@HeaderParam("access_token") String access_token, @HeaderParam("id_token") String id_token, @PathParam("consumer_public_keys") ArrayList<String> consumer_public_keys, @PathParam("active") boolean active) throws ApiException {
+	public com.i3m.api.ApiResponse<ScManagerOasActiveAgreements> checkAgreementsByConsumer(@HeaderParam("access_token") String access_token, @HeaderParam("id_token") String id_token, @PathParam("consumer_public_keys") List<String> consumer_public_keys, @PathParam("active") boolean active) throws ApiException {
 		return new BackplaneClient().checkAgreementsByConsumer(access_token, id_token, (ScManagerOasPublicKeysArray) consumer_public_keys, active);
 	}
 	
@@ -1280,7 +1280,7 @@ public class SdkRiHub {
 	@ApiOperation(value = "retrieve the agreement by provider", tags = "common-services: contract")
 	@ApiResponses(value = { @ApiResponse(code = 400, message = "failed to get the agreements") })
 	@Produces({"application/json"})
-	public com.i3m.api.ApiResponse<ScManagerOasActiveAgreements> checkAgreementsByProvider(@HeaderParam("access_token") String access_token, @HeaderParam("id_token") String id_token, @PathParam("provider_public_keys") ArrayList<String> provider_public_keys, @PathParam("active") boolean active) throws ApiException {
+	public com.i3m.api.ApiResponse<ScManagerOasActiveAgreements> checkAgreementsByProvider(@HeaderParam("access_token") String access_token, @HeaderParam("id_token") String id_token, @PathParam("provider_public_keys") List<String> provider_public_keys, @PathParam("active") boolean active) throws ApiException {
 		return new BackplaneClient().checkAgreementsByProvider(access_token, id_token, (ScManagerOasPublicKeysArray) provider_public_keys, active);
 	}
 	
